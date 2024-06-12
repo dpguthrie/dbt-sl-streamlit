33ed58b0299f489cce11f91d2e79cd031d8d6ce8
Doug Guthrie <douglas.p.guthrie@gmail.com>
2024-06-11 19:24:13 -0600

    Revert "Update LLM to use Langgraph"

--- pages/02_🧠_LLM.py
+++ pages/02_🧠_LLM.py
@@ -1,21 +1,21 @@
 # stdlib
-import time
 
 # third party
 import streamlit as st
-from langchain_community.chat_message_histories import StreamlitChatMessageHistory
-from langchain_core.messages import AIMessage
-from langchain_core.runnables import RunnableConfig
-from langchain_core.tracers import LangChainTracer
-from langchain_core.tracers.run_collector import RunCollectorCallbackHandler
-from langchain_openai import ChatOpenAI
-from langsmith import Client
-from streamlit_feedback import streamlit_feedback
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+from langchain.output_parsers import PydanticOutputParser
+from langchain.prompts import PromptTemplate
+from langchain.prompts.few_shot import FewShotPromptTemplate
+from langchain.schema.output_parser import OutputParserException
+from pydantic.v1.error_wrappers import ValidationError
 
 # first party
 from client import get_query_results
-from helpers import create_tabs, to_arrow_table
-from llm.graph import create_graph
+from helpers import create_graphql_code, create_tabs, to_arrow_table
+from llm.examples import EXAMPLES
+from llm.prompt import EXAMPLE_PROMPT
+from schema import Query
 
 st.set_page_config(
     page_title="dbt Semantic Layer - View Metrics",
@@ -28,7 +28,7 @@ if "conn" not in st.session_state or st.session_state.conn is None:
     st.warning("Go to home page and enter your JDBC URL")
     st.stop()
 
-if "db" not in st.session_state or st.session_state.db is None:
+if "metric_dict" not in st.session_state:
     st.warning(
         "No metrics found.  Ensure your project has metrics defined and a production "
         "job has been run successfully."
@@ -36,141 +36,98 @@ if "db" not in st.session_state or st.session_state.db is None:
     st.stop()
 
 
-# Initialize LangChain client
-langchain_endpoint = "https://api.smith.langchain.com"
-client = Client(api_url=langchain_endpoint, api_key=st.secrets["LANGCHAIN_API_KEY"])
-ls_tracer = LangChainTracer(project_name="default", client=client)
-run_collector = RunCollectorCallbackHandler()
-cfg = RunnableConfig()
-cfg["callbacks"] = [ls_tracer, run_collector]
+def set_openai_api_key():
+    st.session_state._openai_api_key = st.session_state.openai_api_key
 
 
-msgs = StreamlitChatMessageHistory(key="chat_history")
+def set_question():
+    previous_question = st.session_state.get("_question", None)
+    st.session_state._question = st.session_state.question
+    st.session_state.refresh = not previous_question == st.session_state._question
 
-st.write("# Conversational Analytics")
+
+st.write("# LLM Query Builder")
 
 st.markdown(
-    """
-Welcome to the natural language interface to your data!  Start asking questions like:
-- What metrics can I query?
-- What dimensions are available for revenue?
-- Show me the top 10 customers by revenue
-- What's a semantic model?
-"""
+    "Input your OpenAI API Key below and ask questions abour your data.\n\n**This is highly experimental** "
+    "and not meant to handle every edge case.  Please feel free to report any issues (or open up a PR to fix)."
 )
 
-reset_history = st.sidebar.button("Reset chat history")
-if len(msgs.messages) == 0 or reset_history:
-    msgs.clear()
-    msgs.add_ai_message("How can I help you?")
-    st.session_state["last_run"] = None
-
-avatars = {"human": "user", "ai": "assistant"}
-human_messages = []
-for msg in msgs.messages:
-    if msg.type == "human":
-        st.chat_message(avatars[msg.type]).write(msg.content)
-        human_messages.append(msg.content)
-    else:
-        if (
-            "route" in msg.additional_kwargs
-            and msg.additional_kwargs["route"] == "query"
-        ):
-            # create tabs
-            create_tabs(st.session_state, msg.additional_kwargs["run_id"])
-        else:
-            st.chat_message(avatars[msg.type]).write(msg.content)
-
-st.sidebar.text_input(
-    "Databricks API Token",
+api_key = st.text_input(
+    label="OpenAI API Key",
     type="password",
-    key="api_key",
-    value=st.session_state.get("api_key", ""),
+    value=st.session_state.get("_openai_api_key", ""),
+    placeholder="Enter your OpenAI API Key",
+    key="openai_api_key",
+    on_change=set_openai_api_key,
 )
 
-model = ChatOpenAI(
-    model="gpt-4o", temperature=0, openai_api_key=st.secrets["OPENAI_API_KEY"]
+question = st.text_input(
+    label="Ask a question",
+    placeholder="e.g. What is total revenue?",
+    key="question",
+    on_change=set_question,
 )
 
+metrics = ", ".join(list(st.session_state.metric_dict.keys()))
+dimensions = ", ".join(list(st.session_state.dimension_dict.keys()))
 
-def _is_none_or_blank(input: str):
-    return input is None or input.strip() == ""
+parser = PydanticOutputParser(pydantic_object=Query)
 
+prompt_example = PromptTemplate(
+    template=EXAMPLE_PROMPT,
+    input_variables=["metrics", "dimensions", "question", "result"],
+)
 
-if input := st.chat_input(placeholder="What is total revenue?"):
-    if _is_none_or_blank(st.session_state.api_key):
-        st.error("Databricks API Token is required.")
+prompt = FewShotPromptTemplate(
+    examples=EXAMPLES,
+    example_prompt=prompt_example,
+    prefix="""Given a question involving a user's data, transform it into a structured query object.
+                It's important to remember that in the 'orderBy' field, only one of 'metric' or 'groupBy' should be set, not both.
+                Additionally, when adding items to the `where` field and the identifier contains multiple dunder (__) characters,
+                you'll need to change how you specify the dimension.  An example of this is `customer_order__customer__customer_market_segment`.
+                This needs to be represented as Dimension('customer__customer_market_segment').
+                Another example is `order__customer__nation__nation_name`.  This needs to be represented as
+                Dimension('nation__nation_name').  Use only the last primary_entity__dimension_name in the identifier.
+                Here are some examples showing how to correctly and incorrectly structure a query based on a user's question.
+                {format_instructions}
+            """,
+    suffix="Metrics: {metrics}\nDimensions: {dimensions}\nQuestion: {question}\nResult:\n",
+    input_variables=["metrics", "dimensions", "question"],
+    partial_variables={"format_instructions": parser.get_format_instructions()},
+)
+
+if question and st.session_state.get("refresh", False):
+    if st.session_state.get("_openai_api_key", None) is None:
+        st.warning("Please enter your OpenAI API Key")
         st.stop()
-    msgs.add_user_message(input)
-    st.chat_message("user").write(input)
-    graph = create_graph()
-    inputs = {
-        "input": input,
-        "chat_history": human_messages,
-        "model": model,
-        "retriever": st.session_state.db.as_retriever(),
-    }
-    with st.status("Thinking...", expanded=True) as status:
-        data = graph.invoke(inputs, cfg)
-    run_id = run_collector.traced_runs[0].id
-    route = data["route"]
-    with st.chat_message("assistant"):
-        if route == "query":
-            query = data["response"]["query"]
-            payload = {
-                "query": query.gql,
-                "variables": query.variables,
-            }
-            data = get_query_results(payload, source="streamlit-llm")
-            df = to_arrow_table(data["arrowResult"])
-            df.columns = [col.lower() for col in df.columns]
-            setattr(st.session_state, f"query_{run_id}", query)
-            setattr(st.session_state, f"df_{run_id}", df)
-            setattr(st.session_state, f"compiled_sql_{run_id}", data["sql"])
-            create_tabs(st.session_state, run_id)
-            msgs.add_ai_message(
-                AIMessage(
-                    content=str(query),
-                    additional_kwargs={"run_id": run_id, "route": "query"},
-                )
-            )
-        else:
-            try:
-                key = list(data["response"].keys())[0]
-            except KeyError:
-                content = "I'm sorry, I don't have an answer for that."
-            else:
-                content = data["response"][key]
-            st.markdown(content)
-            msgs.add_ai_message(
-                AIMessage(
-                    content=content,
-                    additional_kwargs={"run_id": run_id, "route": route},
-                )
-            )
-        st.session_state.last_run = run_id
-
-
-@st.cache_data(ttl="2h", show_spinner=False)
-def get_run_url(run_id):
-    time.sleep(1)
-    return client.read_run(run_id).url
-
-
-if st.session_state.get("last_run"):
-    # run_url = get_run_url(st.session_state.last_run)
-    # st.sidebar.markdown(f"[Latest Trace: 🛠️]({run_url})")
-    feedback = streamlit_feedback(
-        feedback_type="thumbs",
-        optional_text_label="[Optional] Please provide an explanation",
-        key=f"feedback_{st.session_state.last_run}",
-    )
-    if feedback:
-        scores = {"👎": 0, "👍": 1}
-        client.create_feedback(
-            st.session_state.last_run,
-            feedback["type"],
-            score=scores[feedback["score"]],
-            comment=feedback.get("text", None),
+    try:
+        llm = OpenAI(
+            openai_api_key=st.session_state._openai_api_key,
+            model_name="gpt-3.5-turbo-1106",
+            temperature=0,
         )
-        st.toast("Feedback recorded!", icon="📝")
+    except ValidationError as e:
+        st.write(e)
+        st.stop()
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+    output = chain.run(metrics=metrics, dimensions=dimensions, question=question)
+    try:
+        query = parser.parse(output)
+    except OutputParserException as e:
+        st.error(e)
+        st.stop()
+    python_code = create_graphql_code(query)
+    with st.expander("View API Request", expanded=False):
+        st.code(python_code, language="python")
+    payload = {"query": query.gql, "variables": query.variables}
+    data = get_query_results(payload, source="streamlit-llm")
+    df = to_arrow_table(data["arrowResult"])
+    df.columns = [col.lower() for col in df.columns]
+    st.session_state.query_llm = query
+    st.session_state.df_llm = df
+    st.session_state.compiled_sql_llm = data["sql"]
+
+st.session_state.refresh = False
+create_tabs(st.session_state, "llm")
