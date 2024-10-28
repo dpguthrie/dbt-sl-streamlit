b99b0ab45cc8f25bb44575b8128f7531a20bf7ad
Doug Guthrie <douglas.p.guthrie@gmail.com>
2024-10-28 12:51:45 -0600

    More refined chat

--- pages/02_🧠_LLM.py
+++ pages/02_🧠_LLM.py
@@ -2,11 +2,11 @@
 import streamlit as st
 from langchain.chains import SimpleSequentialChain
 from langchain.chat_models import init_chat_model
-from langchain.output_parsers import PydanticOutputParser, StrOutputParser
 from langchain.prompts import PromptTemplate
 from langchain.schema.output_parser import OutputParserException
 from langchain_community.chat_message_histories import StreamlitChatMessageHistory
 from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
+from langchain_core.output_parsers import PydanticOutputParser, StrOutputParser
 from langchain_core.runnables import RunnableConfig
 from langchain_core.tracers import LangChainTracer
 from langchain_core.tracers.run_collector import RunCollectorCallbackHandler
@@ -15,8 +15,8 @@ from pydantic.v1.error_wrappers import ValidationError
 
 # first party
 from client import get_query_results
-from helpers import create_graphql_code, create_tabs, to_arrow_table
-from llm.prompt import EXAMPLE_PROMPT, intent_prompt, query_prompt, rephrase_prompt
+from helpers import create_tabs, to_arrow_table
+from llm.prompt import intent_prompt, query_prompt, rephrase_prompt
 from llm.providers import MODELS
 from schema import Query
 
@@ -115,7 +115,7 @@ llm = init_chat_model(
     model_name,
     model_provider=provider_name,
     temperature=0,
-    api_key=st.session_state._llm_api_key,
+    api_key=st.session_state.get("_llm_api_key", ""),
 )
 
 if len(msgs.messages) == 0:
@@ -129,7 +129,6 @@ for msg in msgs.messages:
         st.chat_message(avatars[msg.type]).write(msg.content)
         human_messages.append(msg.content)
     else:
-        # Change to tool_call
         if (
             "route" in msg.additional_kwargs
             and msg.additional_kwargs["route"] == "query"
@@ -137,9 +136,6 @@ for msg in msgs.messages:
             create_tabs(st.session_state, msg.additional_kwargs["run_id"])
         else:
             st.chat_message(avatars[msg.type]).write(msg.content)
-
-metrics = ", ".join(list(st.session_state.metric_dict.keys()))
-dimensions = ", ".join(list(st.session_state.dimension_dict.keys()))
 
 rephrase_chain = rephrase_prompt | llm | StrOutputParser()
 intent_chain = intent_prompt | llm | StrOutputParser()
@@ -159,31 +155,40 @@ if input := st.chat_input(placeholder="What is total revenue in June?"):
         st.write("Determining intent...")
         intent = intent_chain.invoke({"question": question}, cfg)
         if intent == "query":
-            st.write("Querying data...")
+            st.write("Creating semantic layer request...")
             query = query_chain.invoke(
                 {
-                    "metrics": metrics,
-                    "dimensions": dimensions,
+                    "metrics": ", ".join(list(st.session_state.metric_dict.keys())),
+                    "dimensions": ", ".join(
+                        list(st.session_state.dimension_dict.keys())
+                    ),
                     "question": question,
                 }
             )
-            run_id = run_collector.traced_runs[0].id
             payload = {"query": query.gql, "variables": query.variables}
+            st.write("Querying semantic layer...")
             data = get_query_results(payload, source="streamlit-llm")
             df = to_arrow_table(data["arrowResult"])
             df.columns = [col.lower() for col in df.columns]
+            run_id = run_collector.traced_runs[0].id
             setattr(st.session_state, f"query_{run_id}", query)
             setattr(st.session_state, f"df_{run_id}", df)
             setattr(st.session_state, f"compiled_sql_{run_id}", data["sql"])
-            create_tabs(st.session_state, run_id)
-            msgs.add_ai_message(
-                AIMessage(
-                    content=str(query),
-                    additional_kwargs={"run_id": run_id, "route": "query"},
-                )
-            )
         else:
             st.write("Retrieving metadata...")
             pass
+        status.update(label="Complete!", expanded=False, state="complete")
 
     human_messages.append(HumanMessage(content=input))
+
+    if intent == "query":
+
+        create_tabs(st.session_state, run_id)
+        msgs.add_ai_message(
+            AIMessage(
+                content=str(query),
+                additional_kwargs={"run_id": run_id, "route": "query"},
+            )
+        )
+    else:
+        pass
